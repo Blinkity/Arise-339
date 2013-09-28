4a6b5aa27147abd4ba5c6e78aad23cd4ebade6e5
Nodira <nodira.k@gmail.com>
2013-09-28 14:02:00 -0700

    pretty sky. pretty plane.

---
+++ Assets/QuirkyPlaneMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuirkyPlaneMover : MonoBehaviour {
+	public VirtualJoystick virtualJoystick; 
+	public int speed; 
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(virtualJoystick.GetAxis("Vertical") * 4, virtualJoystick.GetAxis("Horizontal") * 4, 0);  
+		transform.Translate( new Vector3(0, 0, speed) * Time.deltaTime); 
+		
+		
+	}
+	
+}
