9fad5aa94f87a16fff1d12b9fd6fb04e9b6f669a
Alexander Jaffe Laptop <ajaffe@gamail.com>
2013-09-28 12:53:26 -0700

    Added Virtual Joystic and stub pilot.

--- Assets/Scripts/Pilot.cs
+++ Assets/Scripts/Pilot.cs
@@ -1,17 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Transformer : MonoBehaviour {
+public class Pilot : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		 
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//transform.Translate( new Vector3(0, 0, 1)*Time.deltaTime*50); 
-		transform.Rotate(Vector3.up*1);
+		
 	}
-	
 }
