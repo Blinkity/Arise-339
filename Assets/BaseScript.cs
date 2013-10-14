4ff7baab8f5f289b6cb5ed6ce8919c0d3aab73e8
Nodira <nodira.k@gmail.com>
2013-10-13 22:05:29 -0700

    added some files. lots of them.

---
+++ Assets/BaseScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = SharedVariables.baseLocation; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SharedVariables.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SharedVariables : MonoBehaviour {
+	
+	public static Vector3 baseLocation = new Vector3(0,0,300);
+	public static float heightAtWhichToLanded = 50f;
+	
+	public static float maxDistanceFromHome = 200f; 
+	
+	public static float baseWidth = 100f; 
+	public static float baseLength = 100f; 
+	
+	public static Vector3 landingLocation() {
+		return new Vector3(baseLocation.x, baseLocation.y + heightAtWhichToLanded, baseLocation.z); 
+		
+	}
+	
+}
